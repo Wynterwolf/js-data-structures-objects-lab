fd601903487a64469a99051ba33d49bd278e24ff
Samantha Jordan <shinyguns@gmail.com>
2021-03-23 18:44:19 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+//define driver varible and assign an object
+let driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    return Object.assign({}, driver, { [key]: value });
+}
+
+//should mutate
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key, value){
+    const newObject = Object.assign({}, driver) // clone
+    delete newObject[key] // delete
+    return newObject // return object
+}
+
+//should mutate
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key]
+    return driver
+}
+
